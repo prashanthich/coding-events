06af4ca89462ae29fb61da44f6b23edbd3f22a3b
Prashanthi Chirunomula <chirunomulap@gmail.com>
2021-01-03 16:30:32 -0600

    Display events changed arraylist to hashmap & ul to table

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,12 +18,17 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    //private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
+        events.put("Harry Styles", "Event on September 15, 2021 @Enterprise Center");
+        events.put("Andrea Bocelli","Event on October 17, 2021 @Enterprise Center");
+        events.put("New year celebrations","Downtown Arch");
         model.addAttribute("events", events);
+
         return "events/index";
     }
 
@@ -34,7 +40,7 @@ public class EventController {
 
     @PostMapping("create")
     public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+        //events.add(eventName);
         return "redirect:";
     }
 
